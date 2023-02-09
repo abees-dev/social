9a009eaea76b947f9fa63c405537bac5241577b3
abees <abeesdevjs@gmail.com>
2023-02-10 03:31:15 +0700

    setup project

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,3 @@
 tar --exclude="node_modules" --exclude="build" --exclude=".env" --exclude=".github"  --exclude=".idea" --exclude="deploy"   -zcvf SERVICE.tgz *
 cp SERVICE.tgz ./deploy && rm -rf SERVICE.tgz
 cd ./deploy && chmod +x deploy.sh
-./deploy.sh
